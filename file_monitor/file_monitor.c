00e5bcd7206399c125881c6aff8491e3589a7757
ziv <north780316@gamil.com>
2020-02-19 09:05:11 +0800

    [Description] update recursive watch for robust
    [Commit Files]
    file_monitor/file_monitor.c
    
    -------------------------------------------

--- file_monitor/file_monitor.c
+++ file_monitor/file_monitor.c
@@ -25,6 +25,10 @@
 #define EVENT_SIZE (sizeof(struct inotify_event))
 #define EVENT_BUF_LEN (1024 * (EVENT_SIZE + 16))
 
+bool is_init_mutex = 0;
+pthread_mutex_t g_lock;
+int g_total_watch_dir_count = 1;
+
 // =========================================
 // File monitor API
 // =========================================
@@ -32,7 +36,14 @@
 static void *thread_dir_monitor_inotify ( void *arg )
 {
 	char *dir_path = (char *) arg;
-	printf( "[Monitor] create thread TID=%ld to monitor %s\n", syscall( SYS_gettid ), dir_path );
+
+	pthread_mutex_lock( &g_lock );
+
+	g_total_watch_dir_count += 1;
+	printf( "[Monitor] n_watch_dir=%d create thread TID=%ld to monitor %s\n", g_total_watch_dir_count, syscall( SYS_gettid ), dir_path );
+
+	pthread_mutex_unlock( &g_lock );
+
 	dir_monitor_inotify( dir_path );
 	return NULL;
 }
@@ -48,17 +59,17 @@ void file_monitor_inotify ( char *file_path )
 		abort();
 	}
 
-		// add file to monitor
-		// IN_ACCESS: read, exec
-		// IN_MODIFY: write, truncate
-		// IN_DELETE_SELF: monitor file be delete
-		uint32_t mask = IN_ACCESS | IN_MODIFY | IN_OPEN | IN_DELETE_SELF;
-		int wd = inotify_add_watch( fd, file_path, mask );
-		if ( -1 == wd )
-		{
-			fprintf( stderr, "[Error] in %s: inotify_add_watch fail -> %s\n", __func__, strerror(errno) );
-			abort();
-		}
+	// add file to monitor
+	// IN_ACCESS: read, exec
+	// IN_MODIFY: write, truncate
+	// IN_DELETE_SELF: monitor file be delete
+	uint32_t mask = IN_ACCESS | IN_MODIFY | IN_OPEN | IN_DELETE_SELF;
+	int wd = inotify_add_watch( fd, file_path, mask );
+	if ( -1 == wd )
+	{
+		fprintf( stderr, "[Error] in %s: inotify_add_watch fail -> %s\n", __func__, strerror(errno) );
+		abort();
+	}
 
 	char event_buf[EVENT_BUF_LEN];
 	while ( true )
@@ -110,6 +121,15 @@ end_monitor:
 
 void dir_monitor_inotify ( char *watch_dir )
 {
+	// initialize mutex for recursive watch by multi-threading
+	if ( g_opts.recursive_watch )
+	{
+		if ( !is_init_mutex )
+		{
+			pthread_mutex_init( &g_lock, NULL );
+		}
+	}
+
 	// create inotify instance
 	int init_flag = 0; // flag can be IN_NONBLOCK or IN_CLOEXEC
 	int fd = inotify_init1( init_flag ); // flag = 0 then inotify_init1 is the same to inotify_init
@@ -123,7 +143,18 @@ void dir_monitor_inotify ( char *watch_dir )
 	// IN_ACCESS: read, exec
 	// IN_MODIFY: write, truncate
 	// IN_DELETE_SELF: monitor file be delete
-	uint32_t mask = IN_ACCESS | IN_MODIFY | IN_OPEN | IN_CREATE | IN_DELETE | IN_DELETE_SELF;
+	uint32_t mask = IN_ACCESS | // read
+	                IN_MODIFY | // write
+			IN_OPEN |   // open
+			IN_CREATE | // creat
+			IN_DELETE | // unlink event file
+			IN_DELETE_SELF | // unlink watch target
+			IN_CLOSE_WRITE | // close on write file
+			IN_CLOSE_NOWRITE | // close on read file
+			IN_MOVED_TO | // event file move into watch dir
+			IN_MOVED_FROM | // event file move out from watch dir
+			IN_MOVE_SELF | // watch target move
+			IN_ATTRIB; // chmod
 	int wd = inotify_add_watch( fd, watch_dir, mask );
 	if ( -1 == wd )
 	{
@@ -144,145 +175,186 @@ void dir_monitor_inotify ( char *watch_dir )
 			event = (struct inotify_event *) &event_buf[i];
 			++n_event;
 		}
-		printf( "[Monitor] read %ld event\n", n_event );
+		if ( g_opts.debug )
+		{
+			printf( "[Monitor] read %ld event\n", n_event );
+		}
 
 		// show events
 		for ( ssize_t i = 0; i < n_read; i += (EVENT_SIZE + event->len) )
 		{
 			event = (struct inotify_event *) &event_buf[i];
 
-			int is_dir = -1;
-			int is_exist = -1;
-			char *event_file_type = NULL;
+			bool is_dir;
+			char *event_file_type = "directory"; // watch target directory
 			if ( 0 != event->len ) 
 			{
+				// event file is under watch directory
 				sprintf( event_file_path, "%s/%s", watch_dir, event->name );
 
-				is_exist = check_file_exist( event_file_path );
-
-				if ( is_exist )
+				is_dir = event->mask & IN_ISDIR;
+				if ( is_dir )
 				{
-					is_dir = check_is_dir( event_file_path );
-					if ( 1 == is_dir )
+					event_file_type = "directory";
+				}
+				else 
+				{
+					event_file_type = "file";
+				}
+
+				if ( event->mask & IN_CREATE )
+				{
+					printf( "[Monitor] %s %s be create\n", event_file_type, event_file_path );
+
+					if ( is_dir && g_opts.recursive_watch )
 					{
-						event_file_type = "directory";
+						pthread_t tid;
+						int status;
+						pthread_attr_t thread_attr;
+						status = pthread_attr_init( &thread_attr );
+						if ( 0 != status )
+						{
+							fprintf( stderr, "[Error] pthread_attr_init fail -> %s\n", strerror(errno) );
+							abort();
+						}
+
+						char *sub_dir_path = (char *) malloc( (strlen(event_file_path) + 1) * sizeof(char) );
+						strcpy( sub_dir_path, event_file_path );
+						status = pthread_create( &tid, &thread_attr, thread_dir_monitor_inotify, sub_dir_path );
+						if ( 0 != status )
+						{
+							fprintf( stderr, "[Error] pthread_create fail -> %s\n", strerror(errno) );
+							abort();
+						}
 					}
-					else if ( 0 == is_dir )
+				}
+				else if ( event->mask & IN_DELETE )
+				{
+					printf( "[Monitor] %s %s be delete\n", event_file_type, event_file_path );
+				}
+				else if ( event->mask & IN_OPEN )
+				{
+					if ( !is_dir )
 					{
-						event_file_type = "file";
+						printf( "[Monitor] %s %s be opened\n", event_file_type, event_file_path );
 					}
-					else
+				}
+				else if ( event->mask & IN_ACCESS )
+				{
+					if ( !is_dir )
 					{
-						// -1
-						// file be delete imediately after check file exist
-						event_file_type = "entry";
+						printf( "[Monitor] %s %s be read accessed\n", event_file_type, event_file_path );
 					}
+				}
+				else if ( event->mask & IN_MODIFY )
+				{
+					if ( !is_dir )
+					{
+						printf( "[Monitor] %s %s be write modified\n", event_file_type, event_file_path );
+					}
+				}
+				else if ( event->mask & IN_CLOSE_WRITE )
+				{
+					if ( !is_dir )
+					{
+						printf( "[Monitor] %s %s be write close\n", event_file_type, event_file_path );
+					}
+				}
+				else if ( event->mask & IN_CLOSE_NOWRITE )
+				{
+					if ( !is_dir )
+					{
+						printf( "[Monitor] %s %s be read close\n", event_file_type, event_file_path );
+					}
+				}
+				else if ( event->mask & IN_MOVED_TO )
+				{
+					printf( "[Monitor] %s %s be move into %s\n", event_file_type, event_file_path, watch_dir );
+
+					if ( is_dir && g_opts.recursive_watch )
+					{
+						pthread_t tid;
+						int status;
+						pthread_attr_t thread_attr;
+						status = pthread_attr_init( &thread_attr );
+						if ( 0 != status )
+						{
+							fprintf( stderr, "[Error] pthread_attr_init fail -> %s\n", strerror(errno) );
+							abort();
+						}
+
+						char *sub_dir_path = (char *) malloc( (strlen(event_file_path) + 1) * sizeof(char) );
+						strcpy( sub_dir_path, event_file_path );
+						status = pthread_create( &tid, &thread_attr, thread_dir_monitor_inotify, sub_dir_path );
+						if ( 0 != status )
+						{
+							fprintf( stderr, "[Error] pthread_create fail -> %s\n", strerror(errno) );
+							abort();
+						}
+					}
+				}
+				else if ( event->mask & IN_MOVED_FROM )
+				{
+					printf( "[Monitor] %s %s be move out from %s\n", event_file_type, event_file_path, watch_dir );
+				}
+				else if ( event->mask & IN_ATTRIB )
+				{
+					printf( "[Monitor] %s %s be chmod\n", event_file_type, event_file_path );
 				}
 				else
 				{
-					// file be delete imediately after read event
-					event_file_type = "entry";
-				}
-			}
-
-			if ( event->mask & IN_CREATE )
-			{
-				if ( 0 == event->len )
-				{
-					fprintf( stderr, "[Error] event->len=0 when file create in monitored directory\n" );
-					abort();
-				}
-
-				printf( "[Monitor] %s %s be create\n", event_file_type, event_file_path );
-
-				if ( (1 == is_dir) && g_opts.recursive_watch )
-				{
-					pthread_t tid;
-					int status;
-					pthread_attr_t thread_attr;
-					status = pthread_attr_init( &thread_attr );
-					if ( 0 != status )
+					if ( g_opts.debug )
 					{
-						fprintf( stderr, "[Error] pthread_attr_init fail -> %s\n", strerror(errno) );
-						abort();
-					}
-					status = pthread_create( &tid, &thread_attr, thread_dir_monitor_inotify, event_file_path );
-					if ( 0 != status )
-					{
-						fprintf( stderr, "[Error] pthread_create fail -> %s\n", strerror(errno) );
-						abort();
+						fprintf( stderr, "[Warning] ignore event mask %0#10x\n", event->mask );
 					}
 				}
-			}
-			else if ( event->mask & IN_DELETE )
-			{
-				if ( 0 == event->len )
-				{
-					fprintf( stderr, "[Error] event->len=0 when file delete in monitored directory\n" );
-					abort();
-				}
-				printf( "[Monitor] entry %s be delete\n", event_file_path );
-			}
-			else if ( event->mask & IN_OPEN )
-			{
-				if ( 0 == event->len )
-				{
-					printf( "[Monitor] directory %s be opened\n", watch_dir );
-				}
-				else
-				{
-					printf( "[Monitor] %s %s be opened\n", event_file_type, event_file_path );
-				}
-			}
-			else if ( event->mask & IN_ACCESS )
-			{
-				if ( 0 == event->len )
-				{
-					printf( "[Monitor] directory %s be accessed\n", watch_dir );
-				}
-				else
-				{
-					printf( "[Monitor] %s %s be accessed\n", event_file_type, event_file_path );
-				}
-			}
-			else if ( event->mask & IN_MODIFY )
-			{
-				if ( 0 == event->len )
-				{
-					printf( "[Monitor] directory %s be modified\n", watch_dir );
-				}
-				else
-				{
-					printf( "[Monitor] %s %s be modified\n", event_file_type, event_file_path );
-				}
-			}
-			else if ( event->mask & IN_DELETE_SELF )
-			{
-				printf( "[Monitor] watch target %s be delete\n", watch_dir );
-				goto end_monitor;
-			}
-			else if ( event->mask & IN_IGNORED )
-			{
-				printf( "[Monitor] watch target %s be ignored\n", watch_dir ); // follow by IN_DELETE_SELF
-				goto end_monitor;
 			}
 			else
 			{
-				fprintf( stderr, "[Warning] unknow event mask %0#10x\n", event->mask );
+				// event file is watch directory self
+				if ( event->mask & IN_ATTRIB )
+				{
+					printf( "[Monitor] watch target %s be chmod\n", watch_dir );
+				}
+				else if ( event->mask & IN_DELETE_SELF )
+				{
+					printf( "[Monitor] watch target %s be delete\n", watch_dir );
+					goto end_monitor;
+				}
+				else if ( event->mask & IN_IGNORED )
+				{
+					printf( "[Monitor] watch target %s be ignored\n", watch_dir ); // follow by IN_DELETE_SELF
+					goto end_monitor;
+				}
+				else if ( event->mask & IN_MOVE_SELF )
+				{
+					printf( "[Monitor] watch target %s be move\n", watch_dir ); 
+					goto end_monitor;
+				}
+				else
+				{
+					if ( g_opts.debug )
+					{
+						fprintf( stderr, "[Warning] ignore event mask %0#10x\n", event->mask );
+					}
+				}
 			}
 		}
-
 	}
 
 end_monitor:
 
-	printf( "[Monitor] end of monitor\n" );
+	printf( "[Monitor] stop monitor %s\n", watch_dir );
 
 	inotify_rm_watch( fd, wd );
 
 	// close inotify instance
 	close( fd );
 
+	// re-count
+	pthread_mutex_lock( &g_lock );
+	g_total_watch_dir_count -= 1;
+	pthread_mutex_unlock( &g_lock );
 }
 
 void file_monitor_fanotify ( char *file_path )
